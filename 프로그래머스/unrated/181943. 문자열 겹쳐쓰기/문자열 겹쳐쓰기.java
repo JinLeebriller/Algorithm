ee06c1bb4bca4791e40c85730084cfbc7f58e187
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-03 15:16:00 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 1.59 ms, Memory: 77.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,12 +1,7 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        String surplus = "";
-
-        surplus = my_string.substring(s + overwrite_string.length());
-        my_string = my_string.replaceAll(my_string.substring(s), overwrite_string);
-        answer += my_string + surplus;
-
-        return answer;
+        String before = my_string.substring(s + overwrite_string.length());
+        String after = my_string = my_string.replaceAll(my_string.substring(s), overwrite_string);
+        return after + before;
     }
 }
