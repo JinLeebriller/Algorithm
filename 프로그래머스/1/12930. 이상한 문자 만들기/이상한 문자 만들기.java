7dee361663aa2bb1d74a98955a90caf7e688d4fe
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-08 16:39:30 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 2.44 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -5,23 +5,16 @@ class Solution {
 
         for(int i = 0 ; i < words.length ; i++) {
             StringBuilder sb = new StringBuilder(words[i]);
-
             for(int j = 0 ; j < sb.length() ; j++) {
-                if (j % 2 == 0) {
-                    sb.setCharAt(j, Character.toUpperCase(sb.charAt(j)));
-                } else {
-                    sb.setCharAt(j, Character.toLowerCase(sb.charAt(j)));
-                }
+                sb.setCharAt(j, j % 2 == 0 ? Character.toUpperCase(sb.charAt(j)) : Character.toLowerCase(sb.charAt(j)));
             }
-
             words[i] = sb.toString();
         }
 
         for(int i = 0 ; i < words.length ; i++) {
+            answer += words[i];
             if(i < words.length - 1) {
-                answer += words[i] + " ";
-            } else if(i == words.length - 1) {
-                answer += words[i];
+                answer += " ";
             }
         }
 
