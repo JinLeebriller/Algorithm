7d41ac74bbb06bcdf3ad51a98d97c0c5cdeb9ab8
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-09 16:45:30 +0900

    [level 1] Title: 시저 암호, Time: 35.23 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,32 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
     public String solution(String s, int n) {
         String answer = "";
         char[] sCharArray = s.toCharArray();
-        List<Character> lowerCase = new ArrayList<>();
-        List<Character> upperCase = new ArrayList<>();
-
-        for(int i = 0, j = 97, k = 65 ; i < 26 ; i++, j++, k++) {
-            lowerCase.add((char)j);
-            upperCase.add((char)k);
-        }
 
         for(int i = 0 ; i < sCharArray.length ; i++) {
-            if(lowerCase.contains(sCharArray[i])) {
-                if((char)(sCharArray[i] + n) > 122) {
-                    sCharArray[i] = (char)(96 + ((sCharArray[i] + n) - 122));
+            if(97 <= sCharArray[i] && sCharArray[i] <= 122) {
+                if ((char) (sCharArray[i] + n) > 122) {
+                    sCharArray[i] = (char) (96 + ((sCharArray[i] + n) - 122));
                 } else {
-                    sCharArray[i] = (char)(sCharArray[i] + n);
-                }
-            } else if(upperCase.contains(sCharArray[i])) {
-                if((char)(sCharArray[i] + n) > 90) {
-                    sCharArray[i] = (char)(64 + ((sCharArray[i] + n) - 90));
-                } else {
-                    sCharArray[i] = (char)(sCharArray[i] + n);
+                    sCharArray[i] = (char) (sCharArray[i] + n);
                 }
             }
+
+            if(65 <= sCharArray[i] && sCharArray[i] <= 90) {
+                if ((char) (sCharArray[i] + n) > 90) {
+                    sCharArray[i] = (char) (64 + ((sCharArray[i] + n) - 90));
+                } else {
+                    sCharArray[i] = (char) (sCharArray[i] + n);
+                }
+            }
+
             answer += String.valueOf(sCharArray[i]);
         }
 
