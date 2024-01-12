48c31fd1ab009dd17fb0a4ea3a672ca53f66a049
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-12 12:44:08 +0900

    [level Lv.2] Title: 카펫, Time: 0.03 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.2/42842. 카펫/카펫.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        List<Integer> aliquot = new ArrayList<>();
+
+        for(int i = 1 ; i <= brown + yellow ; i++) {
+            if((brown + yellow) % i == 0) {
+                aliquot.add(i);
+            }
+        }
+
+        for(int i = aliquot.size() - 1, j = 0 ; i >= 0 ; i--, j++) {
+            if((aliquot.get(i) - 2)*(aliquot.get(j) - 2) == yellow) {
+                answer[0] = aliquot.get(i);
+                answer[1] = aliquot.get(j);
+                break;
+            }
+        }
+
+        return answer;
+    }
+}
