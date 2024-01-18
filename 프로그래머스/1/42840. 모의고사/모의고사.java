e7dc58f6525e0d7a3261c4621cd9d1f32560f1e4
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-18 18:30:07 +0900

    [level 1] Title: 모의고사, Time: 1.50 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,33 +2,29 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[][] student = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
-
-        Map<Integer, Integer> studentMap = new HashMap<>();
-        studentMap.put(1, 0);
-        studentMap.put(2, 0);
-        studentMap.put(3, 0);
+        int[] students = {0, 0, 0};
+        int[][] studentsAnswers = {{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
 
         for(int i = 0, s1 = 0, s2 = 0, s3 = 0 ; i < answers.length ; i++, s1++, s2++, s3++) {
-            if(s1 == student[0].length) s1 = 0;
-            if(s2 == student[1].length) s2 = 0;
-            if(s3 == student[2].length) s3 = 0;
+            if(s1 == studentsAnswers[0].length) s1 = 0;
+            if(s2 == studentsAnswers[1].length) s2 = 0;
+            if(s3 == studentsAnswers[2].length) s3 = 0;
 
-            if(answers[i] == student[0][s1]) studentMap.put(1, studentMap.get(1) + 1);
-            if(answers[i] == student[1][s2]) studentMap.put(2, studentMap.get(2) + 1);
-            if(answers[i] == student[2][s3]) studentMap.put(3, studentMap.get(3) + 1);
+            if(answers[i] == studentsAnswers[0][s1]) students[0]++;
+            if(answers[i] == studentsAnswers[1][s2]) students[1]++;
+            if(answers[i] == studentsAnswers[2][s3]) students[2]++;
         }
 
-        int temp = studentMap.get(1);
+        int temp = 0;
         List<Integer> highScore = new ArrayList<>();
 
-        for(int i = 1 ; i <= studentMap.size() ; i++) {
-            if(temp < studentMap.get(i)) {
-                temp = studentMap.get(i);
+        for(int i = 0 ; i <= 2 ; i++) {
+            if(temp < students[i]) {
+                temp = students[i];
                 highScore.removeAll(highScore);
-                highScore.add(i);
-            } else if(temp == studentMap.get(i)) {
-                highScore.add(i);
+                highScore.add(i + 1);
+            } else if(temp == students[i]) {
+                highScore.add(i + 1);
             }
         }
 
