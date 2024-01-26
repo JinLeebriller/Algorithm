de96053a4ba9dd2c5ddc09b1ac901c7c3fdb0eb9
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-26 16:26:39 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,8 +1,7 @@
 class Solution {
     static boolean[] visited;
 
-    // dfs - 재귀함수 사용
-    public static int solution(int n, int[][] computers) {
+    public int solution(int n, int[][] computers) {
         int answer = 0;
         visited = new boolean[n];
 
@@ -16,7 +15,7 @@ class Solution {
         return answer;
     }
 
-    static void dfs(int depth, int[][] computers, int n) {
+    void dfs(int depth, int[][] computers, int n) {
         visited[depth] = true;
 
         for(int i = 0 ; i < n ; i++) {
