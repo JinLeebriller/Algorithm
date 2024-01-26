b518ae81e8aeedbfcd887292989310dbcf2cd96a
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-26 15:37:30 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 83.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,8 +1,5 @@
-import java.util.*;
-
 class Solution {
     static boolean[] visited;
-    static Queue<Integer> queue = new LinkedList<>();
     
     public int solution(int n, int[][] computers) {
         int answer = 0;
@@ -11,25 +8,19 @@ class Solution {
         for(int i = 0 ; i < n ; i++) {
             if(!visited[i]) {
                 answer++;
-                bfs(i, computers, n);
+                dfs(i, computers, n);
             }
         }
 
         return answer;
     }
     
-    void bfs(int depth, int[][] computers, int n) {
+    void dfs(int depth, int[][] computers, int n) {
         visited[depth] = true;
-        queue.offer(depth);
 
-        while(!queue.isEmpty()) {
-            int w = queue.poll();
-
-            for(int i = 0 ; i < n ; i++) {
-                if(!visited[i] && computers[w][i] != 0) {
-                    visited[i] = true;
-                    queue.offer(i);
-                }
+        for(int i = 0 ; i < n ; i++) {
+            if(!visited[i] && computers[i][depth] != 0) {
+                dfs(i, computers, n);
             }
         }
     }
