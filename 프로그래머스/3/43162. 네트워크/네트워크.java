c8ee0685f106db666018723b4fd21e65d1b55882
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-01-26 14:12:08 +0900

    [level 3] Title: 네트워크, Time: 0.09 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,27 @@
+class Solution {
+    static boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        visited = new boolean[computers.length];
+
+        for(int i = 0 ; i < n ; i++) {
+            if(!visited[i]) {
+                answer++;
+                dfs(i, computers, n);
+            }
+        }
+
+        return answer;
+    }
+    
+    void dfs(int depth, int[][] computers, int n) {
+        visited[depth] = true;
+
+        for(int i = 0 ; i < n ; i++) {
+            if(!visited[i] && computers[i][depth] != 0) {
+                dfs(i, computers, n);
+            }
+        }
+    }
+}
