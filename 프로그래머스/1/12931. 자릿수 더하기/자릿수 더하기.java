302600408f3f1d82eb9ef4e8f150feabfdb438ba
Jinhyeong Lee <137128382+JinLeebriller@users.noreply.github.com>
2024-02-18 18:02:47 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 71.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder(String.valueOf(n));
+
+        for(int i = 0 ; i < sb.length() ; i++) {
+            answer += Integer.parseInt(String.valueOf(sb.charAt(i)));
+        }
+
+        return answer;
+    }
+}
